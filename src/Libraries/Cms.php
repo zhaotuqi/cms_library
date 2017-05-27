65173b676cc396e254f90410ccbf22465ab7c677
huajian.liu <huajian.liu@wenba100.com>
2017-05-27 17:50:43 +0800

    测试

--- src/Libraries/Cms.php
+++ src/Libraries/Cms.php
@@ -8,10 +8,32 @@
 
 namespace App\Libraries;
 
+use Illuminate\Support\Facades\Redis;
+
 class Cms
 {
+    //cms在redis中的config_key
+    const CMS_CONFIG_KEY = 'cms_keys';
+
+    protected $redisCMS;
+
+    public function __construct()
+    {
+        echo "=======";
+        $this->redisCMS = Redis::connection('cms');
+    }
+
     public function test()
     {
         echo "测试通过啦！\n";
+        Redis::connection('cms');
+//        var_dump($this->redisCMS);
+    }
+
+    public function getConfigFromCache($configId, $tag)
+    {
+        $key = 'cms_' . $configId . '_' . $tag;
+
+//        return json_decode($this->redisCMS->hGet(CMS_CONFIG_KEY, $key), true);
     }
 }
