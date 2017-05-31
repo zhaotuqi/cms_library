7fa3714a75d50d55bbddc1669cf3bcde71cb7fd0
huajian.liu <huajian.liu@wenba100.com>
2017-05-31 17:17:58 +0800

    增加更新redis

--- src/Libraries/Cms.php
+++ src/Libraries/Cms.php
@@ -8,6 +8,8 @@
 
 namespace App\Libraries;
 
+use Common;
+
 defined('CMS_REDIS_DB') or define('CMS_REDIS_DB', [
     'qa' => [
         'host'     => '10.2.1.28',
@@ -21,6 +23,11 @@ defined('CMS_REDIS_DB') or define('CMS_REDIS_DB', [
         'port'     => 6379,
         'database' => 0
     ]
+]);
+
+defined('CMS_SERVER') or define('CMS_SERVER', [
+    'qa' => 'http://cms-qa02.xuebadev.com/v1/configs/',
+    'on' => 'http://10.10.84.32:86/v1/configs/'
 ]);
 
 defined('CMS_CONFIG_KEY') or define('CMS_CONFIG_KEY', 'cms_keys');
@@ -48,6 +55,12 @@ class Cms
         $config = $this->configCMS[$configKey];
         $key    = 'cms_' . $config[0] . '_' . $config[1];
 
-        return json_decode($this->redisCMS->hget(CMS_CONFIG_KEY, $key), true);
+        $data = $this->redisCMS->hget(CMS_CONFIG_KEY, $key);
+        if (!$data) {
+            @Common::query(CMS_SERVER[config('cms.env')] . $config[0] . '/' . $config[1], []);
+            $data = $this->redisCMS->hget(CMS_CONFIG_KEY, $key);
+        }
+
+        return json_decode($data, true);
     }
 }
