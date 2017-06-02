64b9ef29742e3fc6f88c3e3a7a64badac901e858
huajian.liu <huajian.liu@wenba100.com>
2017-06-02 15:20:07 +0800

    修改env获取

--- src/Config/cms.php
+++ src/Config/cms.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'env' => 'qa',  //cms运行环境,[qa测试、pro线上]
-    'qa' => [
+    'env' => env('REDIS_CMS_ENV', 'qa'),  //cms运行环境,[qa测试、pro线上]
+    'qa'  => [
         //TODO: 在这填写qa环境下的配置eg:
         //'自定义访问的KEY' => [configId, tag]
         //'DEFAULT_RESEARCHER_EMAIL' => ['3', '1.0']
--- src/Providers/CmsProvider.php
+++ src/Providers/CmsProvider.php
@@ -21,6 +21,14 @@ class CmsProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([__DIR__ . '/../Config/cms.php' => config_path('cms.php')]);
+        $content = file_get_contents(base_path('.env'));
+        if (false == strrpos($content, 'REDIS_CMS_ENV')) {
+            if (App()->environment() == 'production' || App()->environment() == 'pro') {
+                file_put_contents(base_path('.env'), PHP_EOL . 'REDIS_CMS_ENV=pro' . PHP_EOL, FILE_APPEND);
+            } else {
+                file_put_contents(base_path('.env'), PHP_EOL . 'REDIS_CMS_ENV=qa' . PHP_EOL, FILE_APPEND);
+            }
+        }
     }
 
     /**
